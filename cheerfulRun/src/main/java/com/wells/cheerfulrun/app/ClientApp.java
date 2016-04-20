7de9eb76b72193f394b1fee040b548749232c230
wells <guowang1102@163.com>
2016-04-20 09:49:11 +0800

    添加activity栈管理

--- cheerfulRun/src/main/java/com/wells/cheerfulrun/app/ClientApp.java
+++ cheerfulRun/src/main/java/com/wells/cheerfulrun/app/ClientApp.java
@@ -1,12 +1,17 @@
 package com.wells.cheerfulrun.app;
 
+import android.app.Activity;
 import android.app.Application;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by wei on 16/4/17.
  */
 public class ClientApp extends Application {
     private static ClientApp instance;
+    public static List<Activity> activityList = new ArrayList<Activity>();
 
     public static ClientApp getInstance(){
         return instance;
@@ -16,5 +21,11 @@ public class ClientApp extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
+    }
+
+    public void exit(){
+        for(Activity activity:activityList){
+            activity.finish();
+        }
     }
 }
