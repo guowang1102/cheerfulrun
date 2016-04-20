0ff7934943e93f4cbcd8f008a6417cf54b2dae1c
wells <guowang1102@163.com>
2016-04-20 20:42:24 +0800

    modify

--- cheerfulRun/src/main/java/com/wells/cheerfulrun/app/ClientApp.java
+++ cheerfulRun/src/main/java/com/wells/cheerfulrun/app/ClientApp.java
@@ -1,17 +1,12 @@
 package com.wells.cheerfulrun.app;
 
-import android.app.Activity;
 import android.app.Application;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by wei on 16/4/17.
  */
 public class ClientApp extends Application {
     private static ClientApp instance;
-    public static List<Activity> activityList = new ArrayList<Activity>();
 
     public static ClientApp getInstance(){
         return instance;
@@ -23,10 +18,5 @@ public class ClientApp extends Application {
         instance = this;
     }
 
-    public void exit(){
-        for(Activity activity:activityList){
-            activity.finish();
-        }
 
-    }
 }
